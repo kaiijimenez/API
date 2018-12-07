e9358dc8ee9b1c861f541ae70bdee7c49f76bd25
Karina Jimenez Sanchez <karina.jimenez@globant.com>
2018-12-07 17:54:14 -0600

    problemas con convey

---
+++ weatherAPI/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "WeatherAPI/weatherAPI/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+	logs.Info("App started")
+}
